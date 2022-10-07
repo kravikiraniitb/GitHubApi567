043469decb8fdfb009024b474aceb76ab52b2f26
Ravi Kiran Kunsoth <44895351+kravikiraniitb@users.noreply.github.com>
2022-10-07 04:19:18 -0400

    Update TestGitHubApi567Code.py

--- TestGitHubApi567Code.py
+++ TestGitHubApi567Code.py
@@ -5,10 +5,10 @@ from GitHubApi567Code import githubapi567
 
 class TestAPI(unittest.TestCase):
     def testAPI1(self): 
-        self.assertEqual(githubapi567("kravikiraniitb"),None)
+        self.assertEqual(githubapi567("richkempinski"),None)
 
     def testAPI2(self): 
-        self.assertEqual(githubapi567("kravikiraniitb2"),"error occured while retrieving a user's repositories")
+        self.assertEqual(githubapi567("richkempinski2"),"error occured while retrieving a user's repositories")
         #invalid repository name passed
 
 
