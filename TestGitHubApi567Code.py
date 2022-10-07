26165edd72548affd798e2fd121c627dffd54ff1
Ravi Kiran Kunsoth <44895351+kravikiraniitb@users.noreply.github.com>
2022-10-07 04:16:22 -0400

    Update TestGitHubApi567Code.py

--- TestGitHubApi567Code.py
+++ TestGitHubApi567Code.py
@@ -5,7 +5,7 @@ from GitHubApi567Code import githubapi567
 
 class TestAPI(unittest.TestCase):
     def testAPI1(self): 
-        self.assertEqual(githubapi567("kravikiraniitb"),"Results obtained successfully")
+        self.assertEqual(githubapi567("kravikiraniitb"),None)
 
     def testAPI2(self): 
         self.assertEqual(githubapi567("kravikiraniitb2"),"error occured while retrieving a user's repositories")
