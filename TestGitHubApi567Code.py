f4f4345ff3cd7051e8780bd0f3265306b6dae48c
Ravi Kiran Kunsoth <44895351+kravikiraniitb@users.noreply.github.com>
2022-10-13 02:57:45 -0400

    Update TestGitHubApi567Code.py

--- TestGitHubApi567Code.py
+++ TestGitHubApi567Code.py
@@ -1,14 +1,53 @@
 import unittest
 
+from unittest.mock import patch, MagicMock
+#import json
+
 from GitHubApi567Code import githubapi567
 
-
 class TestAPI(unittest.TestCase):
-    def testAPI1(self): 
-        self.assertEqual(githubapi567("richkempinski"),None)
 
-    def testAPI2(self): 
-        self.assertEqual(githubapi567("richkempinski2"),"error occured while retrieving a user's repositories")
+    @patch('GitHubApi567Code.githubapi567')
+    def testAPI1(self, mock_requests):
+        mock_requests.return_value = None
+        self.assertEqual(githubapi567("kravikiraniitb"),None)
+
+    @patch('GitHubApi567Code.githubapi567')
+    def testAPI2(self, mock_requests):
+        mock_requests.return_value = "error occured while retrieving a user's repositories"
+        #self.assertEqual(githubapi567("richkempinski"),None)
+        self.assertEqual(githubapi567("kravikiraniitb2"),"error occured while retrieving a user's repositories")
+
+
+    #@patch('GitHubApi567Code.repo_req')
+    #@patch('GitHubApi567Code.repo_req')
+
+   # @patch.multiple('GitHubApi567Code', repo_req=MagicMock(return_value="<Response [200]>"),commit_req=MagicMock(return_value="<Response [200]>"))
+
+    #def testAPI1(self, **mocks):
+        #mock_repo.return_value = "<Response [200]>"
+        #mock_commit.return_value = "<Response [200]>"
+     #   self.assertEqual(githubapi567("richkempinski"),None)
+
+
+    #@patch('GitHubApi567Code.requests')
+   # def testAPI1(self, mock_requests): 
+      #  mock_response = MagicMock()
+       # #mock_response = "<Response [200]>"
+
+        #mock_response.status_code = 200
+       # mock_response.json.return_value = {"Sample" : "JSON"}
+       # mock_requests.get.return_value = mock_response
+       # self.assertEqual(githubapi567("richkempinski"),None)
+
+
+    #@patch('GitHubApi567Code.requests')
+    #def testAPI2(self, mock_requests): 
+     #   mock_response = MagicMock()
+      #  mock_response.status_code = 404
+       # mock_response.json.return_value = {"Sample" : "JSON"}
+        #mock_requests.get.return_value = mock_response
+       # self.assertEqual(githubapi567("richkempinski2"),"error occured while retrieving a user's repositories")
         #invalid repository name passed
 
 
